bb780b2ccabafd8fdeb3f9bac9d5bd43ab3c6970
diudiulucy <351328420@qq.com>
2017-04-20 21:12:35 +0800

    【rebuild】重构逻辑

--- app/root.js
+++ app/root.js
@@ -17,22 +17,30 @@ export default class Root extends React.Component {
     constructor(props) {
         super(props);   //这一句不能省略，照抄即可
         this.state = {
+            dataSource:new ListView.DataSource({
+                rowHasChanged:(row1,row2)=> row1 !== row2,
+            }),
             loaded:false,
             selectedTab:'微信'
         };
         // 在ES6中，如果在自定义的函数里使用了this关键字，则需要对其进行“绑定”操作，否则this的指向不对
         // 像下面这行代码一样，在constructor中使用bind是其中一种做法（还有一些其他做法，如使用箭头函数等）
-        // this.fetchData = this.fetchData.bind(this);
+        this.fetchData = this.fetchData.bind(this);
     }
     componentDidMount() {
-        setTimeout(
-            () => {
+        this.fetchData();
+    }
+    fetchData() {
+        fetch(REQUEST_URL)
+            .then((response) => response.json())
+            .then((responseData) => {
+                // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
                 this.setState({
-                    loaded:true
-                })
-            }   ,
-            1000
-        );
+                    dataSource:this.state.dataSource.cloneWithRows(responseData.movies),
+                    loaded: true,
+                });
+            })
+            .done();
     }
     render() {
         if(!this.state.loaded){
@@ -62,7 +70,7 @@ export default class Root extends React.Component {
                         renderIcon={() => <Image style={styles.icon} source={require("./img/weixin.png")} />}
                         renderSelectedIcon={() => <Image style={styles.icon} source={require("./img/weixinSel.png")} />}
                         onPress={() => this.setState({ selectedTab: '微信' })}>
-                        <WeiXin/>
+                        <WeiXin dataSource={this.state.dataSource}/>
                     </TabNavigator.Item>
                     <TabNavigator.Item
                         selected={this.state.selectedTab === '通讯录'}
